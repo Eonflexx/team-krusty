f67c115a524a327cbc453c773a28aec7b7edfec7
Noble <Hecaretha@gmail.com>
2020-10-27 21:27:28 -0400

    update to router.push for existing check

---
+++ server-express-mysql/models/rel/association.js
@@ -0,0 +1,15 @@
+/* table not built -----> order_products
+module.exports = function(models) {
+    models.product.belongsToMany(models.order, 
+        { 
+            through: models.order_products,
+            foreignKey: 'products_id'
+        });
+    models.order.belongsToMany(models.products,
+        {
+            through: models.order_products,
+            foreignKey: 'order_id'
+        });
+}
+
+*/
--- server-express-mysql/routes/index.js
+++ server-express-mysql/routes/index.js
@@ -9,52 +9,33 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-/* hbs test
-router.get('/products', function(req, res, next) {
-  models.products.findAll({}).then(productsFound => {
-    res.render('products', {
-      products: productsFound
-    });
+router.get("/products", function(req, res, next) {
+  models.products.findAll().then(products => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(products));
   });
 });
-*/
-
-router.get("/products", function(req, res, next) {
-  models.products.findAll().then(products => res.json(products));
-});
-
 
 router.post('/products', function (req, res, next) {
-  models.products.create(req.body)
-    .then(newProduct => {
+  models.products.findOrCreate({
+    where: { 
+      name: req.body.name, 
+      price: req.body.price 
+    }
+  })
+  .spread(function(newProduct, created) {
+    if (created) {
       res.setHeader('Content-Type', 'application/json');
       res.send(JSON.stringify(newProduct));
-    })
-    .catch(err => {
+    } else {
       res.status(400);
-      res.send(err.message);
-    });
+      res.send('Product already exists');
+      console.log('Product already exists')
+    }
+  })
 });
 
-//////////////////////////////////////////////////
-
-
-///////////////////////////////////////////////////
-/* not working
-router.post("/products", function(req, res, next) {
-  let newProduct = new models.Product();
-  newProduct.name = req.body.name;
-  newProduct.price = req.body.price;
-  newProduct.save().then(product => res.json(product));
-});
-*/
-/* not working
-router.post("/new", function(req, res, next) {
-  models.products.create({
-    name: req.body.name, price: req.body.price
-  }).then(submittedProducts => res.json(submittedProducts));
-});
-*/
+//////////////////////////////////////////
 
 router.delete("/products/:id", function(req, res, next) {
   let productId = parseInt(req.params.id);
@@ -77,3 +58,40 @@ router.put("/:id", function(req, res, next) {
 });
 
 module.exports = router;
+
+/* Working GET hbs test
+router.get('/products', function(req, res, next) {
+  models.products.findAll({}).then(productsFound => {
+    res.render('products', {
+      products: productsFound
+    });
+  });
+});
+*/
+
+/* Working POST without check for existing
+router.post('/products', function (req, res, next) {
+  models.products.create(req.body)
+    .then(newProduct => {
+      res.setHeader('Content-Type', 'application/json');
+      res.send(JSON.stringify(newProduct));
+    })
+    .catch(err => {
+      res.status(400);
+      res.send(err.message);
+    });
+});
+*/
+
+//////////////////////////////////////////////////
+
+
+///////////////////////////////////////////////////
+/* not working
+router.post("/products", function(req, res, next) {
+  let newProduct = new models.products();
+  newProduct.name = req.body.name;
+  newProduct.price = req.body.price;
+  newProduct.save().then(product => res.json(product));
+});
+*/
