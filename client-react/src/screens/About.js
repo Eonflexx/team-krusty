6b2f162846bdb7708d7aa0d05919ab303f4354de
Noble <Hecaretha@gmail.com>
2020-11-02 05:07:01 -0500

    About screen update with Topic componet

--- client-react/src/screens/About.js
+++ client-react/src/screens/About.js
@@ -1,14 +1,29 @@
-import React from 'react';
-import '../App.css';
+import React from "react";
+import Topic from "../components/Topic";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "../App.css";
 
-function About() {
-    return ( 
-        <div>
-            <h1>About Page</h1>
-            <hr/>
-            <h4>Brought to you from Team Krusty</h4>
-        </div>
-    );
+function About({ match }) {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <hr />
+      <h4>Brought to you from Team Krusty</h4>
+      <div>
+        <h2>Meet the Coders</h2>
+        <ul className="Topic-links">
+          <li><Link to={`${match.url}/Noble`}>Lead Back-End product Routing Implementer . . .</Link></li>
+          <li><Link to={`${match.url}/Azzam`}>Lead CSS design and Bootstrap Implementer . . .</Link></li>
+          <li><Link to={`${match.url}/Muhammad`}>Lead Database Migration Implementer . . .</Link></li>
+          <li><Link to={`${match.url}/Muhammad`}>Lead Front-End page routing Implementer . . .</Link></li>
+          <li><Link to={`${match.url}/Hannah`}>Lead ERM Database Model Implementer . . .</Link></li>
+        </ul>
+
+        <Route path={`${match.url}/:topicId`} component={Topic} />
+        <Route exact path={match.url} render={() => <h3>Please select a team member!</h3>} />
+      </div>
+    </div>
+  );
 }
 
 export default About;
--- client-react/src/screens/Nav.js
+++ client-react/src/screens/Nav.js
@@ -19,3 +19,24 @@ function Nav() {
 }
 
 export default Nav;
+
+
+
+/*
+
+// ALTERNATIVE HEADER
+
+import React from 'react';
+
+const Header = ({ title }) => (
+    <div>
+    <a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/topics">Topics</a>
+    <h1>{title}</h1>
+    </div>
+);
+
+export default Header;
+
+*/
