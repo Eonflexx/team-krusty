b0c52802200d9a51bb44af9efabe8015d353ad53
Muhammad <msaleem2300@gmail.com>
2020-11-05 20:34:24 -0500

    cart edit

--- client-react/src/screens/Cart.js
+++ client-react/src/screens/Cart.js
@@ -8,20 +8,10 @@ function Cart({ match }) {
     <div>
       <h1>Cart Page</h1>
       <hr />
-      <h4>Brought to you from Team Krusty</h4>
-      <div>
-        <h2>Meet the Coders</h2>
-        <ul className="Topic-links">
-          <li><Link to={`${match.url}/Noble`}>Lead Back-End product Routing Implementer . . .</Link></li>
-          <li><Link to={`${match.url}/Azzam`}>Lead CSS design and Bootstrap Implementer . . .</Link></li>
-          <li><Link to={`${match.url}/James`}>Lead Database Migration Implementer . . .</Link></li>
-          <li><Link to={`${match.url}/Muhammad`}>Lead Front-End page routing Implementer . . .</Link></li>
-          <li><Link to={`${match.url}/Hannah`}>Lead ERM Database Model Implementer . . .</Link></li>
-        </ul>
-
-        <Route path={`${match.url}/:topicId`} component={Topic} />
-        <Route exact path={match.url} render={() => <h3>Please select a link to see a team member!</h3>} />
-      </div>
+      <p><b>Checkout: </b>Customer Details</p>
+      <label>Name: </label><input></input><br />
+      <label>Address: </label><input></input><br />
+      <label>Click button to accept charges: </label><button>Submit</button>
     </div>
   );
 }
